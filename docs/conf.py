990e0665212ed09b9deea1da59d4e4d605428dc7
exihs <54537576+Existance29@users.noreply.github.com>
2024-05-02 19:13:37 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'pymacauto'
+copyright = '2024'
+author = 'exihs'
 
 release = '0.1'
 version = '0.1.0'
